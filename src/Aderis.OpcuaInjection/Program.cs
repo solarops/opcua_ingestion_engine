02a0049d1bdd84d73ba89ed3d2815587e3613bd9
atbattis <alexbatti1999@gmail.com>
2024-06-11 12:27:32 -0400

    Modified program.cs to DFS for nodes

--- src/Aderis.OpcuaInjection/Program.cs
+++ src/Aderis.OpcuaInjection/Program.cs
@@ -1,15 +1,59 @@
 ﻿using System;
-using System.Threading.Tasks;
+using System.Text;
+using System.Threading;
 using Opc.Ua;
 using Opc.Ua.Client;
 using Opc.Ua.Configuration;
 
 class Program
 {
+
+    private static void DFS(Session session, ReferenceDescription rd, List<ReferenceDescription> NodesToPoll, int numSpaces)
+    {
+        if (rd.NodeClass == NodeClass.Variable)
+        {
+            // Add to polling nodes
+            NodesToPoll.Add(rd);
+        }
+
+
+        if (rd.NodeClass == NodeClass.Variable || rd.NodeClass == NodeClass.Object)
+        {
+            // print
+            // StringBuilder sb = new StringBuilder();
+            // sb.Append(new string(' ', numSpaces));
+            // sb.Append(rd.DisplayName);
+            // sb.Append(", ");
+            // sb.Append(rd.BrowseName);
+            // sb.Append(", ");
+            // sb.Append(rd.NodeClass);
+            // sb.Append(", ");
+            // sb.Append(rd.NodeId);
+            // Console.WriteLine(sb.ToString());
+        }
+
+
+        ReferenceDescriptionCollection nextRefs;
+        byte[] nextCp;
+
+        // Looks for next level 
+        session.Browse(null, null, ExpandedNodeId.ToNodeId(rd.NodeId, session.NamespaceUris), 0u, BrowseDirection.Forward, ReferenceTypeIds.HierarchicalReferences, true, (uint)NodeClass.Variable | (uint)NodeClass.Object, out nextCp, out nextRefs);
+
+        if (nextRefs.Count == 0) return;
+
+        foreach (var nextRd in nextRefs)
+        {
+            DFS(session, nextRd, NodesToPoll, numSpaces + 2);
+            
+        }
+        // return;
+    }
+
     static async Task Main(string[] args)
     {
         // Define the server URL
-        string serverUrl = "opc.tcp://localhost:53530";
+        // string serverUrl = "opc.tcp://localhost:53530";
+        string serverUrl = "opc.tcp://localhost:62541/discovery";
 
         // EXAMPLE is for Anonymous user, no certificate
 
@@ -55,49 +99,25 @@ class Program
                                                       new UserIdentity(new AnonymousIdentityToken()),
                                                       null))
             {
+                // session.TransferSubscriptionsOnReconnect = true;
+
                 Console.WriteLine("Connection to the OPC UA server established successfully.");
 
                 Console.WriteLine("Step 3 - Browse the server namespace.");
                 ReferenceDescriptionCollection refs;
                 Byte[] cp;
-                // session.Browse(null, null, ObjectIds.ObjectsFolder, 0u, BrowseDirection.Forward, ReferenceTypeIds.HierarchicalReferences, true, (uint)NodeClass.Variable | (uint)NodeClass.Object | (uint)NodeClass.Method, out cp, out refs);
-                session.Browse(null, null, ObjectIds.ObjectsFolder, 0u, BrowseDirection.Forward, ReferenceTypeIds.HierarchicalReferences, true, (uint)NodeClass.Variable | (uint)NodeClass.Object, out cp, out refs);
+                session.Browse(null, null, ObjectIds.ObjectsFolder, 0u, BrowseDirection.Forward, ReferenceTypeIds.HierarchicalReferences, true, (uint)NodeClass.Variable | (uint)NodeClass.Object | (uint)NodeClass.Method, out cp, out refs);
+
+                // reference to root of ObjectsFolder
+                // session.Browse(null, null, ObjectIds.ObjectsFolder, 0u, BrowseDirection.Forward, ReferenceTypeIds.HierarchicalReferences, true, (uint)NodeClass.Variable | (uint)NodeClass.Object, out cp, out refs);
                 Console.WriteLine("DisplayName: BrowseName, NodeClass");
 
                 // TODO: evaluate searching algorithms for trees, need to pick NodeClass: Variable (not VariableType)
 
-                foreach (var rd in refs)
-                {
-                    // rd = parent nodes
-                    if (rd.NodeClass == NodeClass.Variable)
-                    {
-                        ExpandedNodeId expandedNodeId = rd.NodeId;
-                        // Can cast ExpandedNodeId to NodeId
-                        NodesToPoll.Add(rd);
-                    }
-
-
-                    // Console.WriteLine("{0}: {1}, {2}, {3}", rd.DisplayName, rd.BrowseName, rd.NodeClass, rd.NodeId);
-                    ReferenceDescriptionCollection nextRefs;
-                    byte[] nextCp;
-                    // session.Browse(null, null, ExpandedNodeId.ToNodeId(rd.NodeId, session.NamespaceUris), 0u, BrowseDirection.Forward, ReferenceTypeIds.HierarchicalReferences, true, (uint)NodeClass.Variable | (uint)NodeClass.Object | (uint)NodeClass.Method, out nextCp, out nextRefs);
-
-                    // Looks for second level 
-                    session.Browse(null, null, ExpandedNodeId.ToNodeId(rd.NodeId, session.NamespaceUris), 0u, BrowseDirection.Forward, ReferenceTypeIds.HierarchicalReferences, true, (uint)NodeClass.Variable | (uint)NodeClass.Object, out nextCp, out nextRefs);
-                    foreach (var nextRd in nextRefs)
-                    {
-                        // nextRd = child nodes
-                        if (nextRd.NodeClass == NodeClass.Variable)
-                        {
-                            ExpandedNodeId expandedNodeId = nextRd.NodeId;
-                            // Can cast ExpandedNodeId to NodeId
-                            NodesToPoll.Add(nextRd);
-                        }
-
-                        // child nodes
-                        // Console.WriteLine("+ {0}: {1}, {2}: {3}", nextRd.DisplayName, nextRd.BrowseName, nextRd.NodeClass, nextRd.NodeId);
-                    }
-                }
+                // foreach (var rd in refs)
+                // {
+                //     DFS(session, rd, NodesToPoll, 0);
+                // }
 
                 Console.WriteLine("########################");
 
@@ -105,29 +125,72 @@ class Program
                 {
                     foreach (var value in item.DequeueValues())
                     {
-                        Console.WriteLine($"Value: {value.Value}, Status: {value.StatusCode}, Timestamp: {value.SourceTimestamp}");
+                        Console.WriteLine($"{item.DisplayName} Value: {value.Value}, Status: {value.StatusCode}, Timestamp: {value.SourceTimestamp}");
                     }
                 }
 
-                
-
-                // var subscription = new Subscription()
+                // List<NodeId> RawNodes = NodesToPoll.Select(n => (NodeId)n.NodeId).ToList();
+                // while (true)
                 // {
-                //     PublishingInterval = 1000, // in milliseconds
-                //     KeepAliveCount = 10,
-                //     LifetimeCount = 1000,
-                //     MaxNotificationsPerPublish = 1000,
-                //     Priority = 0
-                // };
 
-                // session.AddSubscription(subscription);
-                // subscription.Create();
+                //     // Call session.ReadValues to read the values
+                //     DataValueCollection values;
+                //     IList<ServiceResult> errors;
+                //     session.ReadValues(RawNodes, out values, out errors);
+
+                //     foreach (DataValue dataValue in values)
+                //     {
+                //         Console.WriteLine($"Value: {dataValue.Value}");
+                //     }
+
+                //     Thread.Sleep(2000);
+                // }
+
+                var subscription = new Subscription()
+                {
+                    DisplayName = "Console ReferenceClient Subscription",
+                    PublishingEnabled = true,
+                    PublishingInterval = 1000,
+                    LifetimeCount = 0,
+                    MinLifetimeInterval = 120_000,
+                };
+
+                session.AddSubscription(subscription);
+                subscription.Create();
+
+                // BEGIN Custom
+                var monitoredItem = new MonitoredItem(subscription.DefaultItem)
+                {
+                    StartNodeId = "ns=2;s=[default]/Inverter_Tags/PCS_1_1_1/000 - Raw Tags/ACTIVE_POWER",
+                    AttributeId = Attributes.Value,
+                    DisplayName = "power_true_kw",
+                    SamplingInterval = 1000, // ms
+                    QueueSize = 10,
+                    DiscardOldest = true
+                };
+
+                monitoredItem.Notification += OnMonitoredItemNotification;
+                subscription.AddItem(monitoredItem);
+                subscription.ApplyChanges();
+
+                var monitoredItem2 = new MonitoredItem(subscription.DefaultItem)
+                {
+                    StartNodeId = "ns=2;s=[default]/Inverter_Tags/PCS_1_1_1/ACTIVE_POWER",
+                    AttributeId = Attributes.Value,
+                    DisplayName = "power_true_kw_1",
+                    SamplingInterval = 1000, // ms
+                    QueueSize = 10,
+                    DiscardOldest = true
+                };
+
+                monitoredItem2.Notification += OnMonitoredItemNotification;
+                subscription.AddItem(monitoredItem);
+                subscription.ApplyChanges();
+
+                // END Custom
 
                 // foreach (var node in NodesToPoll)
                 // {
-                //     // Console.WriteLine($"Node Id: {node}");
-                //     // DataValue value = session.ReadValue(node);
-                //     // Console.WriteLine($"Node Value: {value.WrappedValue}");
                 //     var monitoredItem = new MonitoredItem(subscription.DefaultItem)
                 //     {
                 //         StartNodeId = (NodeId)node.NodeId,
@@ -138,10 +201,14 @@ class Program
                 //         DiscardOldest = true
                 //     };
 
-                //     monitoredItem.Notification += (item, args) => OnMonitoredItemNotification(item, args);
+                //     monitoredItem.Notification += OnMonitoredItemNotification;
                 //     subscription.AddItem(monitoredItem);
                 //     subscription.ApplyChanges();
                 // }
+
+                // wait for timeout or Ctrl-C
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
             }
         }
         catch (Exception ex)
