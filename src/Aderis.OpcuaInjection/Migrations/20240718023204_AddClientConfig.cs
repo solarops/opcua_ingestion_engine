a72d3b1688459e157fb4aabee65efced05b3303a
atbattis <alexbatti1999@gmail.com>
2024-08-05 11:08:48 -0400

    edited migration to seed data from .json file

--- src/Aderis.OpcuaInjection/Migrations/20240718023204_AddClientConfig.cs
+++ src/Aderis.OpcuaInjection/Migrations/20240718023204_AddClientConfig.cs
@@ -1,10 +1,37 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+using System.Text.Json.Serialization;
+using System.IO;
+using System.Text.Json;
+using Aderis.OpcuaInjection.Helpers;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations.Builders;
 
 #nullable disable
 
 namespace Aderis.OpcuaInjection.Migrations
 {
+    public class LegacyClientList
+    {
+        [JsonPropertyName("connections")]
+        public required List<LegacyClient> Connections { get; set; }
+    }
+
+    public class LegacyClient
+    {
+        [JsonPropertyName("connection_name")]
+        public required string ConnectionName { get; set; }
+        [JsonPropertyName("max_search")]
+        public required int MaxSearch { get; set; }
+        [JsonPropertyName("staleness_timeout_ms")]
+        public required int TimeoutMs { get; set; }
+        [JsonPropertyName("url")]
+        public required string Url { get; set; }
+
+        [JsonPropertyName("browse_exclusion_folders")]
+        public required List<string> Folders { get; set; }
+    }
+
     /// <inheritdoc />
     public partial class AddClientConfig : Migration
     {
@@ -35,7 +62,6 @@ namespace Aderis.OpcuaInjection.Migrations
                 {
                     Id = table.Column<int>(type: "integer", nullable: false)
                         .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    ConnectionOpcClientConnectionId = table.Column<int>(type: "integer", nullable: false),
                     OpcClientConnectionId = table.Column<int>(type: "integer", nullable: false),
                     ExclusionFolder = table.Column<string>(type: "text", nullable: false)
                 },
@@ -54,6 +80,62 @@ namespace Aderis.OpcuaInjection.Migrations
                 name: "IX_BrowseExclusionFolders_OpcClientConnectionId",
                 table: "BrowseExclusionFolders",
                 column: "OpcClientConnectionId");
+
+            var options = new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+            };
+
+            string filePath = Path.Combine("/opt", "sos-config", "opcua_client_config.json");
+
+            if (File.Exists(filePath))
+            {
+                Console.WriteLine("Client Config exists, needs to migrate");
+
+                try
+                {
+                    string rawOutput = OpcuaHelperFunctions.GetFileContentsNoLock(filePath);
+                    var legacyClients = JsonSerializer.Deserialize<LegacyClientList>(rawOutput, options) ?? throw new Exception();
+
+                    foreach (var client in legacyClients.Connections)
+                    {
+                        // new client, start at index
+                        int pgFirstId = 1;
+
+                        // Add to ClientConnections
+                        migrationBuilder.InsertData(
+                            table: "OpcClientConnections",
+                            columns: new[] { "Id", "ConnectionName", "Url", "MaxSearch", "TimeoutMs" },
+                            values: new object[,]
+                            {
+                                { pgFirstId, client.ConnectionName, client.Url, client.MaxSearch, client.TimeoutMs }
+                            });
+
+                        foreach (var folder in client.Folders)
+                        {
+                            migrationBuilder.InsertData(
+                            table: "BrowseExclusionFolders",
+                            columns: new[] { "ExclusionFolder", "OpcClientConnectionId" },
+                            values: new object[,]
+                            {
+                                { folder, pgFirstId }
+                            });
+                        }
+                        pgFirstId += 1;
+                    }
+
+                    // Remove
+                    // File.Delete(filePath);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error when attempting to migrate opcua_client_config: {ex}");
+                    throw new Exception("Failed!");
+                }
+            }
+
+
         }
 
         /// <inheritdoc />
