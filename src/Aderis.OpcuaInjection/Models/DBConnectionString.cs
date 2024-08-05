8d9afb8f9403213fc240c8962d05ff4d58cb6998
atbattis <alexbatti1999@gmail.com>
2024-08-05 12:52:31 -0400

    added default connection parameters

--- src/Aderis.OpcuaInjection/Models/DBConnectionString.cs
+++ src/Aderis.OpcuaInjection/Models/DBConnectionString.cs
@@ -5,23 +5,26 @@ namespace Aderis.OpcuaInjection.Models;
 public class DbConfig
 {
     [JsonPropertyName("modvalues_db_config")]
-    public required DBConnection Connection { get; set; }
+    public DBConnection Connection { get; set; } = new();
     [JsonPropertyName("opcua_client_config")]
-    public required DBConnection ClientConfigConnection { get; set; }
+    public DBConnection ClientConfigConnection { get; set; } = new();
 }
 
+/*
+    Set defaults for access to the db if deserialization doesn't work
+*/
 public class DBConnection
 {
     [JsonPropertyName("server")]
-    public required string Server { get; set; }
+    public string Server { get; set; } = "localhost";
     [JsonPropertyName("port")]
-    public required string Port { get; set; }
+    public string Port { get; set; } = "5432";
     [JsonPropertyName("database")]
-    public required string Database { get; set; }
+    public string Database { get; set; } = "acuity";
     [JsonPropertyName("username")]
-    public required string Username { get; set; }
+    public string Username { get; set; } = "postgres";
     [JsonPropertyName("password")]
-    public required string Password { get; set; }
+    public string Password { get; set; } = "password";
 
     // Host=localhost;Username=myuser;Password=mypassword;Database=mydatabase
     public override string ToString()
