23a86ff96f67b24e5bfce1b33f6868377242c75b
atbattis <alexbatti1999@gmail.com>
2024-08-08 11:02:17 -0400

    Added migration for AutoAcceptFirstUpdate

--- src/Aderis.OpcuaInjection/Migrations/20240808150101_AddAutoAcceptFirstUpdateToConfig.Designer.cs
+++ src/Aderis.OpcuaInjection/Migrations/20240808150101_AddAutoAcceptFirstUpdateToConfig.Designer.cs
@@ -2,6 +2,7 @@
 using Aderis.OpcuaInjection.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Aderis.OpcuaInjection.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240808150101_AddAutoAcceptFirstUpdateToConfig")]
+    partial class AddAutoAcceptFirstUpdateToConfig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Aderis.OpcuaInjection/Migrations/20240808150101_AddAutoAcceptFirstUpdateToConfig.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aderis.OpcuaInjection.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAutoAcceptFirstUpdateToConfig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AutoAcceptFirstUpdate",
+                table: "OpcClientConnections",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AutoAcceptFirstUpdate",
+                table: "OpcClientConnections");
+        }
+    }
+}
